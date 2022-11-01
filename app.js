4cb4782e1af4f8d26212f7456b21b24ed301e37c
Shrinidhi Yadavally <y.shrinidhireddy@gmail.com>
2022-11-01 18:43:35 -0500

    button

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var kiteRouter = require('./routes/kite');
 var selectorRouter = require('./routes/selector');
+var gridbuildRouter = require('./routes/gridbuild');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/kite', kiteRouter);
 app.use('/selector', selectorRouter);
+app.use('/gridbuild', gridbuildRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
